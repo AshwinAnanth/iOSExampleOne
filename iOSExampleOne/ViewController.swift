8f62666be32ce8bd3ef630389d83b5870dc3c7d4
ashwin <mailashwin24@gmail.com>
2017-04-13 07:02:10 -0400

    Initial Commit

---
+++ iOSExampleOne/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSExampleOne
+//
+//  Created by Ashwin A on 2017-04-13.
+//  Copyright © 2017 Ashwin A. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
